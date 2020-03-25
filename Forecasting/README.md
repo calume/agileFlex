3c2d4d1460be7adb34eef2484f738f0b7458505b
Calum Edmunds <qsb15202@uni.strath.ac.uk>
2020-03-25 13:46:31 +0000

    PV rev2

--- Forecasting/README.md
+++ Forecasting/README.md
@@ -13,29 +13,29 @@ on how the data was used to produce distributions and gaussian mixture models.
 
 The script 'forecasting.py' produces forecasts of output from PV.
 
-The function 'Forecast' uses persistence forecast using a previous days data
-then fits gaussian mixtures to produce a range of probable Day Ahead forecasts that 
-can be benchmarked against the persistence forecast (previous days output).
+The function 'Forecast' uses persistence forecast using a previous days data then fits gaussian mixtures (GMM) to produce a best fit Day Ahead forecast that 
+can be benchmarked against the persistence forecast. Both are a form of persistence forecast as they use the previous days output).
 
-The day ahead forecast is refined using the first 10 hours of actual data (i.e. at 10am)
-to produce an intraday forecast.
+The Day Ahead (DA) forecast is refined using the first 10 hours of actual data (i.e. at 10am) to produce an Intraday (ID) forecast.
 
-Below is the day ahead forecast for the 2/7/2014 using data from the day before (1/7/2014).
+Below is the Day Ahead forecast for the 8 days from 2/7/2014 using data from the previous day to form the DA forecast. 
+
+The forecasts are for the site 'Averston Close' which was removed from the training data to allow it to be used for testing.
 
 ![Day Ahead](Day_Ahead.png)
 
-**Figure 1:** PV day ahead forecast 
+**Figure 1:** Alverston Close PV Day Ahead forecast 
 
-The average Day ahead persistence NMAE across all sites is 0.091, and the Day Ahead weighted gaussian mixture NMAE, which is created from the persistence forcast for 2/7/2014 is 0.079. 
+The average Day Ahead persistence Mean Absolute Error (MAE) across all 8 of the days above is 0.136, and the Day Ahead weighted gaussian mixture MAE, which is created from the persistence forcast is 0.111.
 
-The highest day ahead error comes for Alverston Close which has no output on the 2/7/2014. 
+The highest Day Ahead persistence and GMM error comes on Day 1 which has no output. Day 2 also has a very high error as it is using Day 1's output (of 0) to as its estimate when there was in fact a very high output on Day 2.
 
-The intraday forecasts estimated using the first 10 hours of daily output are given below:
+The intraday forecasts estimated using the first 10 hours of daily output, are vital on Days 1 and 2 for correcting this Day Ahead error, these are given below:
 
 ![Intraday](Intraday.png)
 
-The average Intraday gaussian mixture model forecast NMAE across all sites is 0.06. However in most cases the NMAE is worse using the best fit intraday forecast than the day ahead,
-The intraday forecast is most useful for capturing days with no output as occured in Alverston Close.
+The average Intraday GMM forecast MAE across all sites is 0.076. The Intraday GMM provides an improved forecast than the Day Ahead on these 8 days,
+The intraday forecast is most useful for capturing days with no output as occured on Day 1 where the error is reduced from 0.228 to 0.047 from DA to ID.
 
-**Figure 2:** PV intraday forecast
+**Figure 2:** Alverston Close PV intraday forecast
 
--- Forecasting/forecasting.py
+++ Forecasting/forecasting.py
@@ -97,19 +97,20 @@ def forecasts(pred,true,Forecast_Type):
     GMMBestFit= PV_DistsGMMChosen[Season][gmmMAE_Pred.index[0:Fits_Nmin]]
     weights_bestFits=PV_DistsGMMWeights[Season][gmmMAE_Pred.index[0:Fits_Nmin]]
     weights_bestFits=weights_bestFits/weights_bestFits.sum()
-    #print('GMM ' +str(Forecast_Type)+ ' Weighted NMAE with '+str(Fits_Nmin)+' Fits: ',round(WeightedNMAE[Fits_Nmin],3))
+    print('GMM ' +str(Forecast_Type)+ ' Weighted NMAE with '+str(Fits_Nmin)+' Fits: ',round(WeightedNMAE[Fits_Nmin],3))
 
     return GMMBestFit, gmmMAE_True, weights_bestFits, WeightedNMAE[Fits_Nmin],mae
 
 #--------------------Visualise Forecast--------------------------#
 
     #Find best BMM fit
-def visualise_forecast(GMMBestFit,Forecast_Type,weights_bestFits,Site,NMAE):
-    plt.title(Site+', Error - '+str(round(NMAE,3)),fontsize=9)
+def visualise_forecast(GMMBestFit,Forecast_Type,weights_bestFits,Day,GMM_NMAE,Persistence_NMAE):
+    plt.title('Day '+str(Day+1)+', MAE (Error): GMM-'+str(round(GMM_NMAE,3)),fontsize=9)
     if Forecast_Type=='Day Ahead':   
+        plt.title('Day '+str(Day+1)+', MAE (Error): GMM-'+str(round(GMM_NMAE,3))+' Persistence:'+str(round(Persistence_NMAE,3)),fontsize=9)
         plt.plot(pred.values, label='Persistence Forecast',linestyle='--')
     for i in range(0,len(GMMBestFit)):
-        plt.plot(GMMBestFit[i], linewidth=weights_bestFits[i]*2)
+        plt.plot(GMMBestFit[i], linewidth=weights_bestFits[i]*2) #label='GMM Best Fit', 
     plt.plot(true.values, label='Actual', color='black', linestyle=':', linewidth=2)
     times = ["00:00", "04:00", "08:00", "12:00", "16:00", "20:00", "24:00"]
     #plt.ylabel('PV output normalised',fontsize=8)
@@ -123,36 +124,35 @@ def visualise_forecast(GMMBestFit,Forecast_Type,weights_bestFits,Site,NMAE):
 ### Date stuff ###
 
 startdate =  date(2014,7,1)
-enddate   =  date(2014,7,3)
+enddate   =  date(2014,7,11)
 delta = datetime.timedelta(hours=0.5)
-
+days=range(0,8)
 dt = pd.date_range(startdate, enddate, freq=delta) #datetime steps    
-dt[0:47]
 n=1
-DA_WeightedNMAE=pd.Series(index=PV_DataFrame.keys())
-ID_WeightedNMAE=pd.Series(index=PV_DataFrame.keys())
-DA_PersistanceNMAE=pd.Series(index=PV_DataFrame.keys())
+DA_WeightedNMAE=pd.Series(index=days)
+ID_WeightedNMAE=pd.Series(index=days)
+DA_PersistanceNMAE=pd.Series(index=days)
 DA_weights_bestFits={}
 ID_weights_bestFits={}
 DA_GMMBestFit={}
 ID_GMMBestFit={}
 l=0
-for i in PV_DataFrame.keys():
-    pred=PV_DataFrame[i]['P_Norm'][dt[0:48]]
-    true=PV_DataFrame[i]['P_Norm'][dt[48:96]]
+for i in days:
+    pred=PV_DataFrame['Alverston Close']['P_Norm'][dt[i*48:(i+1)*48]]
+    true=PV_DataFrame['Alverston Close']['P_Norm'][dt[(i+1)*48:(i+2)*48]]
     Forecast_Type='Day Ahead'
     DA_GMMBestFit[i], gmmMAE_True, DA_weights_bestFits[i], DA_WeightedNMAE[i],DA_PersistanceNMAE[i]=forecasts(pred,true,Forecast_Type)
     plt.figure(Forecast_Type)
-    plt.subplot(320+n)
-    visualise_forecast(DA_GMMBestFit[i],'Day Ahead',DA_weights_bestFits[i],i,DA_WeightedNMAE[i])
+    plt.subplot(420+n)
+    visualise_forecast(DA_GMMBestFit[i],'Day Ahead',DA_weights_bestFits[i],i,DA_WeightedNMAE[i],DA_PersistanceNMAE[i])
     if n==1:
         plt.legend(fontsize=8)
     Forecast_Type='Intraday'
     plt.figure(Forecast_Type)
-    plt.subplot(320+n)
+    plt.subplot(420+n)
     Intraday_True=true[0:20]
     ID_GMMBestFit[i], gmmMAE_True, ID_weights_bestFits[i], ID_WeightedNMAE[i],blank=forecasts(Intraday_True,true,'Intraday')
-    visualise_forecast(ID_GMMBestFit[i],'Intraday',ID_weights_bestFits[i],i,ID_WeightedNMAE[i])
+    visualise_forecast(ID_GMMBestFit[i],'Intraday',ID_weights_bestFits[i],i,ID_WeightedNMAE[i],0)
     if n==1:
         plt.legend(fontsize=8)
     n=n+1
